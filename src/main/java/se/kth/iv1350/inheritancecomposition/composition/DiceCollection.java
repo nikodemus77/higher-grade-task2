cddccae9efafa5046870fc3da0b21eb8e5dd8f13
nikodemus77 <niklasedblad@hotmail.com>
2025-05-22 17:27:41 +0200

    added methods to DiceCollection

--- src/main/java/se/kth/iv1350/inheritancecomposition/composition/DiceCollection.java
+++ src/main/java/se/kth/iv1350/inheritancecomposition/composition/DiceCollection.java
@@ -11,9 +11,120 @@ public class DiceCollection {
     public DiceCollection(){
         this.rng = new Random();
     }
+    
+    public void roll(int numberofDice, String typeofDice){
 
-    public void dSixDie(){
-        System.out.println(rng.ints(1,6));
+
+    }
+    // Alla tärningar ska bli privata metoder anropade av metoden roll
+    public void d4(){
+        final int bound = 5;
+
+        int result = rng.nextInt(bound);
+        boolean decided = false;
+
+        while (!decided){
+            if(result == 0){
+                result = rng.nextInt(bound);
+            }
+            if(result != 0){
+                decided = true;
+            }
+
+        }
+        System.out.println("The die shows a: " + result);
+    }
+
+    public void d6(){
+        final int bound = 7;
+
+        int result = rng.nextInt(bound);
+        boolean decided = false;
+
+        while (!decided){
+            if(result == 0){
+                result = rng.nextInt(bound);
+            }
+            if(result != 0){
+                decided = true;
+            }
+
+        }
+        System.out.println("The die shows a: " + result);
+    }
+
+    public void d8(){
+        final int bound = 9;
+
+        int result = rng.nextInt(bound);
+        boolean decided = false;
+
+        while (!decided){
+            if(result == 0){
+                result = rng.nextInt(bound);
+            }
+            if(result != 0){
+                decided = true;
+            }
+
+        }
+        System.out.println("The die shows a: " + result);
+    }
+
+    public void d10(){
+        
+        final int bound = 11;
+
+        int result = rng.nextInt(bound);
+        boolean decided = false;
+
+        while (!decided){
+            if(result == 0){
+                result = rng.nextInt(bound);
+            }
+            if(result != 0){
+                decided = true;
+            }
+
+        }
+        System.out.println("The die shows a: " + result);
+    }
+
+    public void d12(){
+        
+        final int bound = 13;
+
+        int result = rng.nextInt(bound);
+        boolean decided = false;
+
+        while (!decided){
+            if(result == 0){
+                result = rng.nextInt(bound);
+            }
+            if(result != 0){
+                decided = true;
+            }
+
+        }
+        System.out.println("The die shows a: " + result);
+    }
+
+    public void d20(){
+        
+        final int bound = 21;
+
+        int result = rng.nextInt(bound);
+        boolean decided = false;
+
+        while (!decided){
+            if(result == 0){
+                result = rng.nextInt(bound);
+            }
+            if(result != 0){
+                decided = true;
+            }
+        }
+        System.out.println("The die shows a: " + result);
     }
 
     
--- src/main/java/se/kth/iv1350/inheritancecomposition/main/Main.java
+++ src/main/java/se/kth/iv1350/inheritancecomposition/main/Main.java
@@ -1,6 +1,7 @@
 package se.kth.iv1350.inheritancecomposition.main; 
 
 import se.kth.iv1350.inheritancecomposition.composition.*;
+import se.kth.iv1350.inheritancecomposition.inheritance.*;
 
 public class Main {
     public static void main(String[] args){
