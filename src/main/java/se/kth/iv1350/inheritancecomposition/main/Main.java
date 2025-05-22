8a746cd5725bd8a0dfc5e623d1e164ad4755d5a6
nikodemus77 <niklasedblad@hotmail.com>
2025-05-22 17:01:24 +0200

    seperated the files to their own packages

--- src/main/java/se/kth/iv1350/inheritancecomposition/main/Main.java
+++ src/main/java/se/kth/iv1350/inheritancecomposition/main/Main.java
@@ -1,5 +1,7 @@
 package se.kth.iv1350.inheritancecomposition.main; 
 
+import se.kth.iv1350.inheritancecomposition.composition.*;
+
 public class Main {
     public static void main(String[] args){
         DiceCollection diceCol = new DiceCollection();
